372695954fc396c7f26a04a10759e06a1bfcf1e9
nfrik <nfrik@iphonedreams.com>
2019-02-14 20:00:37 -0500

    Initial commit

--- nidaqmx.py
+++ nidaqmx.py
@@ -8,9 +8,11 @@ import logging
 from utils import plott
 from tqdm import tqdm
 from scipy import signal
+import pandas as pd
 
 ttables = {}
 ttables['xor'] = [[-1, -1, 0], [-1, 1, 1], [1, -1, 1], [1, 1, 0]]
+ttables['xor3'] = [[-1, -1, -1, 0],[-1, -1, 1, 1],[-1, 1, -1, 0], [-1, 1, 1, 1],[1, -1, -1, 1],[1, -1, 1, 0],[1, 1, -1, 1],[1, 1, 1, 0]]
 ttables['or'] = [[-1, -1, 0], [-1, 1, 1], [1, -1, 1], [1, 1, 0]]
 ttables['and'] = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]]
 
@@ -32,17 +34,17 @@ def simple_experiment():
         taskin.ai_channels.add_ai_voltage_chan("AI/ai1",terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
         taskin.ai_channels.add_ai_voltage_chan("AI/ai3", terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
         # taskin.timing.cfg_samp_clk_timing(1000,sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
-        t=np.linspace(0,1,60)
+        t=np.linspace(0,1,100)
         sawtooth = 1 + signal.sawtooth(2 * np.pi * 5 * t, width=0.5)
         sawtooth = 3 * np.hstack((sawtooth, -1 * sawtooth))
-        signal1=1.3*np.sin(2*np.pi*3*t)
+        signal1=5*np.sin(2*np.pi*10*t)
         signal2 = 1.3*np.sin(2 * np.pi * 3 * t)
         # signal1=sawtooth
         # signal2=sawtooth
         # signal=0*np.ones(100)
         taskout1.write(signal1, auto_start=True)
         result=[]
-        for s1,s2 in zip(signal1,signal2):
+        for s1,s2 in tqdm(zip(signal1,signal2)):
             # time.sleep(0.01)
             taskout1.write(s1, auto_start=True)
             # taskout2.write(sawtooth, auto_start=True)
@@ -73,7 +75,7 @@ def simple_experiment():
     plt.show()
 
 
-def nidaq_single_sim(X,y, inputids, outputids, eq_time):
+def nidaq_single_sim(X,y, inputids, outputids, eq_time,av_samps=1):
 
     logger.debug("Setting up inputs: {} for outputs: {} ".format(X, y))
 
@@ -89,18 +91,23 @@ def nidaq_single_sim(X,y, inputids, outputids, eq_time):
         logger.info("Done equilibrating, reading output values")
         for outid in outputids:
             taskin.ai_channels.add_ai_voltage_chan("AI/ai{}".format(outid),min_val=-10.0, max_val=10.0,terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
-        result = taskin.read(number_of_samples_per_channel=1)
+
+        result=[]
+        for i in range(av_samps):
+            result.append(np.array(taskin.read(number_of_samples_per_channel=1)).reshape((1,-1))[0])
+            time.sleep(eq_time)
+        result=np.array(result).mean(axis=0)
         outvals.append(list(np.array(result).reshape((1,-1))[0]))
 
         outvals[-1].append(y)
     return outvals[0]
 
 
-def nidaq_circuit_eval(X, y, inputids, outputids, eq_time, zero_each_step=True):
+def nidaq_circuit_eval(X, y, inputids, outputids, eq_time, zero_each_step=True,av_samps=10):
     results = []
 
     for xx,yy in tqdm(zip(X,y)):
-        results.append(nidaq_single_sim(X=xx,y=yy,inputids=inputids,outputids=outputids,eq_time=eq_time))
+        results.append(nidaq_single_sim(X=xx,y=yy,inputids=inputids,outputids=outputids,eq_time=eq_time,av_samps=av_samps))
 
         if zero_each_step:
             with nidaqmx.Task() as taskout:
@@ -120,24 +127,28 @@ def main():
     # print(outvals2)
     # X=[[1,2,3],[-1,-2,-3],[7.23,7.24,-7.25]]
 
-    data = np.array(ttables['xor'] * 20)
+    data = np.array(ttables['xor'] * 10)
     X = data[:, :-1]
     y = data[:, -1]
-    X = perturb_X(X, boost=10., var=0.00)
+    X = perturb_X(X, boost=5.0, var=0.00)
     # n=10
     # # X=np.random.random((n,3))*3
     # X=np.ones((n,3))*5.002
     # y=np.ones(n)
-    result = nidaq_circuit_eval(X,y,inputids=[0,1],outputids=[0,1,2],eq_time=0.05)
+    result = nidaq_circuit_eval(X,y,inputids=[0,1],outputids=[0,1,2],eq_time=0.1,av_samps=5)
     plott.plot3d(result)
+    plott.pca_plotter(result)
     print(X)
     print(np.array(result))
+    df=pd.DataFrame(np.array(result))
+    df.to_csv('result.csv')
+
     # 2.9996851733847802], [2.9996851733847802], [3.0009987072145923]
-    # plt.figure()
-    # plt.subplot(211)
-    # plt.plot(outvals1)
-    # plt.show()
-    # print(outvals2)
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(result)
+    plt.show()
+    print(result)
 
 def main2():
     simple_experiment()
